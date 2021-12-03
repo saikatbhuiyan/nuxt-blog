947b30f35bd5f0932468c8693fe08aff7c29d279
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-12-04 04:19:24 +0600

    create core-components plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   css: ["~assets/styles/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~plugins/core-components.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
